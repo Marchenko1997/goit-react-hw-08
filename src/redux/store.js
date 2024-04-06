7ca49d21be68f8374979b3b4ccfb000c8bd43fdf
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-04-06 14:09:56 +0200

    library redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filters: filtersReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["contacts"], // Зберігаємо лише стан поля contacts
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filters: filtersReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
